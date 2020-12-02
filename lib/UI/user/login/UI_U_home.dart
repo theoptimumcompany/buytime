4ca62a43f8b1663f11d49eff10768426a6761d8e
anupin <rukshannipuna12@gmail.com>
2020-12-02 14:57:43 +0100

    Upadated UI_U_Home

--- lib/UI/user/login/UI_U_home.dart
+++ lib/UI/user/login/UI_U_home.dart
@@ -140,6 +140,7 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
                                 height: double.infinity,
                               ),
                             ),
+
                           ),
                           ///Logo & Buytime text
                           Positioned.fill(
