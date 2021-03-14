91104e206351b9f9ea577607fcf3203a318a7efc
riccardo <serrarik90@gmail.com>
2021-03-14 13:26:22 +0100

    Fix some bug on Pushone

--- lib/reblox/model/service/service_state.g.dart
+++ lib/reblox/model/service/service_state.g.dart
@@ -22,8 +22,8 @@ ServiceState _$ServiceStateFromJson(Map<String, dynamic> json) {
     price: (json['price'] as num)?.toDouble(),
     timesSold: json['timesSold'] as int,
     tag: (json['tag'] as List)?.map((e) => e as String)?.toList(),
-    switchSlots: json['switchSlots'] as bool,
-    switchAutoConfirm: json['switchAutoConfirm'] as bool,
+    switchSlots: json['switchSlots'] as bool ?? false,
+    switchAutoConfirm: json['switchAutoConfirm'] as bool ?? false,
     serviceSlot: (json['serviceSlot'] as List)
         ?.map((e) =>
             e == null ? null : ServiceSlot.fromJson(e as Map<String, dynamic>))
