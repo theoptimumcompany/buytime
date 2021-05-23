fcafa19e5cf4e0a16601d0fb074c18add09d9f58
francesco romeo <f.romeo.f@gmail.com>
2021-05-23 19:58:50 +0200

    pdf update

--- lib/reblox/model/service/service_state.dart
+++ lib/reblox/model/service/service_state.dart
@@ -31,6 +31,8 @@ class ServiceState {
   bool serviceCreated = false;
   @JsonKey(defaultValue: false)
   bool serviceEdited = false;
+  @JsonKey(defaultValue: true)
+  bool serviceExternallyVisible = true;
   String serviceBusinessAddress;
   String serviceBusinessCoordinates;
   String serviceAddress;
@@ -61,6 +63,7 @@ class ServiceState {
     this.spinnerVisibility,
     this.serviceCreated,
     this.serviceEdited,
+    this.serviceExternallyVisible,
     this.serviceBusinessAddress,
     this.serviceBusinessCoordinates,
     this.serviceAddress,
@@ -89,6 +92,7 @@ class ServiceState {
       spinnerVisibility: false,
       serviceCreated: false,
       serviceEdited: false,
+      serviceExternallyVisible: false,
       serviceBusinessAddress: '',
       serviceBusinessCoordinates: '',
       serviceAddress: '',
@@ -117,6 +121,7 @@ class ServiceState {
     this.spinnerVisibility = service.spinnerVisibility;
     this.serviceCreated = service.serviceCreated;
     this.serviceEdited = service.serviceEdited;
+    this.serviceExternallyVisible = service.serviceExternallyVisible;
     this.serviceBusinessAddress = service.serviceBusinessAddress;
     this.serviceBusinessCoordinates = service.serviceBusinessCoordinates;
     this.serviceAddress = service.serviceAddress;
@@ -144,6 +149,7 @@ class ServiceState {
     bool spinnerVisibility,
     bool serviceCreated,
     bool serviceEdited,
+    bool serviceExternallyVisible,
     String serviceBusinessAddress,
     String serviceBusinessCoordinates,
     String serviceAddress,
@@ -170,6 +176,7 @@ class ServiceState {
       spinnerVisibility: spinnerVisibility ?? this.spinnerVisibility,
       serviceCreated: serviceCreated ?? this.serviceCreated,
       serviceEdited: serviceEdited ?? this.serviceEdited,
+      serviceExternallyVisible: serviceExternallyVisible ?? this.serviceExternallyVisible,
       serviceBusinessAddress: serviceBusinessAddress ?? this.serviceBusinessAddress,
       serviceBusinessCoordinates: serviceBusinessCoordinates ?? this.serviceBusinessCoordinates,
       serviceAddress: serviceAddress ?? this.serviceAddress,
