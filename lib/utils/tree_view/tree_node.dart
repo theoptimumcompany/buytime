f13058414053ef20da157ecd99c237e48ca92633
riccardo <serrarik90@gmail.com>
2021-07-16 18:39:53 +0200

    No debug EXPANDED

--- lib/utils/tree_view/tree_node.dart
+++ lib/utils/tree_view/tree_node.dart
@@ -53,7 +53,7 @@ class _TreeNodeState extends State<TreeNode>
 
   @override
   void didChangeDependencies() {
-    debugPrint('EXPANDED 1');
+   // debugPrint('EXPANDED 1');
     super.didChangeDependencies();
     TreeView _treeView = TreeView.of(context);
     _controller.duration = _treeView.theme.expandSpeed;
@@ -67,7 +67,7 @@ class _TreeNodeState extends State<TreeNode>
 
   @override
   void didUpdateWidget(TreeNode oldWidget) {
-    debugPrint('EXPANDED 2');
+   // debugPrint('EXPANDED 2');
     if (widget.node.expanded != oldWidget.node.expanded) {
       setState(() {
         _isExpanded = widget.node.expanded;
@@ -87,7 +87,7 @@ class _TreeNodeState extends State<TreeNode>
   }
 
   void _handleExpand() {
-    debugPrint('EXPANDED 3');
+  //  debugPrint('EXPANDED 3');
     TreeView _treeView = TreeView.of(context);
     assert(_treeView != null, 'TreeView must exist in context');
     setState(() {
@@ -106,7 +106,7 @@ class _TreeNodeState extends State<TreeNode>
   }
 
   void _handleTap() {
-    debugPrint('EXPANDED 4');
+   // debugPrint('EXPANDED 4');
     TreeView _treeView = TreeView.of(context);
     assert(_treeView != null, 'TreeView must exist in context');
     if (_treeView.onNodeTap != null) {
@@ -115,7 +115,7 @@ class _TreeNodeState extends State<TreeNode>
   }
 
   void _handleDoubleTap() {
-    debugPrint('EXPANDED 5');
+  //  debugPrint('EXPANDED 5');
     TreeView _treeView = TreeView.of(context);
     assert(_treeView != null, 'TreeView must exist in context');
     if (_treeView.onNodeDoubleTap != null) {
@@ -124,7 +124,7 @@ class _TreeNodeState extends State<TreeNode>
   }
 
   Widget _buildNodeExpander() {
-    debugPrint('EXPANDED 6');
+  //  debugPrint('EXPANDED 6');
     TreeView _treeView = TreeView.of(context);
     assert(_treeView != null, 'TreeView must exist in context');
     TreeViewTheme _theme = _treeView.theme;
@@ -143,7 +143,7 @@ class _TreeNodeState extends State<TreeNode>
   }
 
   Widget _buildNodeIcon() {
-    debugPrint('EXPANDED 7');
+  //  debugPrint('EXPANDED 7');
     TreeView _treeView = TreeView.of(context);
     assert(_treeView != null, 'TreeView must exist in context');
     TreeViewTheme _theme = _treeView.theme;
@@ -166,7 +166,7 @@ class _TreeNodeState extends State<TreeNode>
   }
 
   Widget _buildNodeLabel() {
-    debugPrint('EXPANDED 8');
+  //  debugPrint('EXPANDED 8');
     TreeView _treeView = TreeView.of(context);
     assert(_treeView != null, 'TreeView must exist in context');
     TreeViewTheme _theme = _treeView.theme;
@@ -335,7 +335,7 @@ class _TreeNodeState extends State<TreeNode>
   }
 
   Widget _buildNodeWidget() {
-    debugPrint('EXPANDED 9');
+ //   debugPrint('EXPANDED 9');
     TreeView _treeView = TreeView.of(context);
     assert(_treeView != null, 'TreeView must exist in context');
     TreeViewTheme _theme = _treeView.theme;
@@ -418,12 +418,12 @@ class _TreeNodeState extends State<TreeNode>
     final bool closed =
         (!_isExpanded || !widget.node.expanded) && _controller.isDismissed;
     final nodeWidget = _buildNodeWidget();
-    debugPrint('EXPANDED 10');
+  //  debugPrint('EXPANDED 10');
     return widget.node.isParent
         ? AnimatedBuilder(
       animation: _controller.view,
       builder: (BuildContext context, Widget child) {
-        debugPrint('EXPANDED 10-1');
+    //    debugPrint('EXPANDED 10-1');
         return Column(
           mainAxisSize: MainAxisSize.min,
           children: <Widget>[
@@ -446,7 +446,7 @@ class _TreeNodeState extends State<TreeNode>
         child: Column(
             mainAxisSize: MainAxisSize.min,
             children: widget.node.children.map((Node node) {
-              debugPrint('EXPANDED 10-2');
+         //     debugPrint('EXPANDED 10-2');
               return Container(
                   padding: EdgeInsets.only(top: 0.0, left: 0),
                   child: TreeNode(node: node)
