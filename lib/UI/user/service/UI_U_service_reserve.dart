50912816a974db96b95d2aa9d33fcb425aa67f62
anupin <rukshannipuna12@gmail.com>
2021-08-03 19:50:02 +0200

    Update real time serivce reserve

--- lib/UI/user/service/UI_U_service_reserve.dart
+++ lib/UI/user/service/UI_U_service_reserve.dart
@@ -744,8 +744,8 @@ class _ServiceReserveState extends State<ServiceReserve> with SingleTickerProvid
                       Provider.of<ReserveList>(context, listen: false).updateSlotIndex(element[0], Provider.of<ReserveList>(context, listen: false).selectedSquareSlotList[element[0]][element[1]][2]);
                       Provider.of<ReserveList>(context, listen: false).updateControllerList(element[0], 0, Provider.of<ReserveList>(context, listen: false).selectedSquareSlotList[element[0]][element[1]][2]);
                       Provider.of<ReserveList>(context, listen: false).updateSlotControllerList(element[0], 0, Provider.of<ReserveList>(context, listen: false).selectedSquareSlotList[element[0]][element[1]][2]);
-                      Provider.of<ReserveList>(context, listen: false)._controllerList[element[0]][1].scrollTo(index: tmpSelectedSquareSlotList[element[0]].length-1, duration: Duration(milliseconds: 500));
-                      Provider.of<ReserveList>(context, listen: false)._slotControllerList[element[0]][1].scrollTo(index: tmpSelectedSquareSlotList[element[0]].length-1, duration: Duration(milliseconds: 500));
+                      Provider.of<ReserveList>(context, listen: false)._controllerList[element[0]][1].jumpTo(index: tmpSelectedSquareSlotList[element[0]].length-1);
+                      Provider.of<ReserveList>(context, listen: false)._slotControllerList[element[0]][1].jumpTo(index: tmpSelectedSquareSlotList[element[0]].length-1);
                     });
 
                   });
@@ -1308,10 +1308,7 @@ class _ServiceReserveState extends State<ServiceReserve> with SingleTickerProvid
                                                                           debugPrint('QUANTITY SLOT: ${reserveState._controllerList[index][0]}');
                                                                           if (reserveState.selectedSquareSlotList[index].length > 1) {
                                                                             debugPrint('MOVE TO');
-                                                                            setState(() {
-
-                                                                            });
-                                                                            //reserveState._controllerList[index][1].scrollTo(index: reserveState._controllerList[index][0], duration: Duration(milliseconds: 500));
+                                                                            reserveState._controllerList[index][1].jumpTo(index: reserveState._controllerList[index][0]);
                                                                             //Provider.of<ReserveList>(context, listen: false)._slotControllerList[index][1].scrollTo(index: i, duration: Duration(milliseconds: 500));
                                                                           }
                                                                           debugPrint('SELECTED SLOT LIST: ${reserveState.selectedSquareSlotList[index].length}');
