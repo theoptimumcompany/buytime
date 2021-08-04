df4922b48700daed025c43ba3d78fc0b37126f59
riccardo <serrarik90@gmail.com>
2021-08-04 17:45:40 +0200

    Fix Order Detail

--- lib/UI/user/booking/widget/user_service_card_widget.dart
+++ lib/UI/user/booking/widget/user_service_card_widget.dart
@@ -1,3 +1,4 @@
+import 'package:Buytime/UI/user/booking/UI_U_order_details.dart';
 import 'package:Buytime/reblox/model/app_state.dart';
 import 'package:Buytime/reblox/model/order/order_state.dart';
 import 'package:Buytime/reblox/model/service/service_state.dart';
