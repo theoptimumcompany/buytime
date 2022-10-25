7b7fb02c8fbdb20d5638ea6104ceebfdf1d5a912
francesco romeo <f.romeo.f@gmail.com>
2022-10-25 12:08:16 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	lib/UI/management/activity/UI_M_activity_management.dart
    #	lib/UI/management/activity/UI_M_activity_management_item_details.dart
    #	lib/UI/management/invite/widget/w_booking_month_list.dart
    #	lib/UI/management/service_external/UI_M_external_service_list.dart
    #	lib/UI/management/service_internal/UI_M_edit_service.dart
    #	lib/UI/management/slot/widget/time_slot_management_widget.dart
    #	lib/UI/user/booking/RUI_U_notifications.dart
    #	lib/UI/user/booking/UI_U_confirm_booking.dart
    #	lib/UI/user/business/UI_U_business_list.dart
    #	lib/UI/user/cart/UI_U_ConfirmOrder.dart
    #	lib/UI/user/cart/UI_U_ConfirmOrder_backup.dart
    #	lib/UI/user/cart/UI_U_add_card.dart
    #	lib/UI/user/cart/tab/T_credit_cards.dart
    #	lib/UI/user/cart/tab/T_native_apple.dart
    #	lib/UI/user/cart/tab/T_native_google.dart
    #	lib/UI/user/cart/tab/T_progress.dart
    #	lib/UI/user/cart/tab/T_room.dart
    #	lib/UI/user/cart/tab/T_room_disabled.dart
    #	lib/UI/user/landing/UI_U_landing.dart
    #	lib/UI/user/landing/test.dart
    #	lib/UI/user/login/UI_U_business_data.dart
    #	lib/UI/user/service/widget/w_time_slot.dart
    #	lib/UI/user/turist/RUI_U_service_explorer.dart
    #	lib/UI/user/turist/UI_U_service_explorer.dart
    #	lib/environment_abstract.dart
    #	lib/helper/payment/util.dart
    #	lib/main.dart
    #	lib/reblox/reducer/app_reducer.dart
    #	lib/reusable/appbar/w_buytime_appbar.dart
    #	lib/reusable/back_button_blue.dart
    #	lib/reusable/booking_page_service_list_item.dart
    #	lib/reusable/branded_button_tourist.dart
    #	lib/reusable/business/optimum_business_card_medium_user.dart
    #	lib/reusable/container_shape_top_circle.dart
    #	lib/reusable/easy_check_in.dart
    #	lib/reusable/enterExitRoute.dart
    #	lib/reusable/error_dialog.dart
    #	lib/reusable/form/optimum_dropdown.dart
    #	lib/reusable/icon/buytime_icons.dart
    #	lib/reusable/icon/material_design_icons.dart
    #	lib/reusable/image_cropper.dart
    #	lib/reusable/order/optimum_order_history_item_card_medium.dart
    #	lib/reusable/order/order_total.dart
    #	lib/reusable/order/w_optimum_order_item_card_medium.dart
    #	lib/reusable/stripe/optimum_credit_card_button.dart
    #	lib/reusable/stripe/show_dialog_to_dismiss.dart
    #	lib/reusable/w_landing_card.dart
    #	lib/reusable/w_promo_discount.dart
    #	lib/services/file_upload_service_web.dart
    #	lib/storage/fire_storage_service.dart
    #	lib/storage/mobile_storage.dart
    #	lib/storage/unsupported_storage.dart
    #	lib/storage/web_storage.dart

---
+++ lib/reusable/w_convention_discount.dart
@@ -0,0 +1,56 @@
+import 'package:Buytime/reblox/model/service/service_state.dart';
+import 'package:Buytime/reusable/icon/material_design_icons.dart';
+import 'package:Buytime/helper/convention/convention_helper.dart';
+import 'package:Buytime/utils/size_config.dart';
+import 'package:Buytime/utils/theme/buytime_theme.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:Buytime/reblox/model/app_state.dart';
+
+class W_ConventionDiscount extends StatefulWidget {
+  bool onlyIcon;
+  ServiceState serviceState;
+  String businessId;
+  W_ConventionDiscount(this.serviceState, this.businessId, this.onlyIcon);
+  @override
+  _W_ConventionDiscountState createState() => _W_ConventionDiscountState();
+}
+
+class _W_ConventionDiscountState extends State<W_ConventionDiscount> {
+  @override
+  Widget build(BuildContext context) {
+
+    ConventionHelper conventionHelper = ConventionHelper();
+    return Container(
+        height: 20,
+        //width: 130,
+        ///SizeConfig.safeBlockHorizontal * 40
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(6),
+          color: BuytimeTheme.AccentRed,
+        ),
+        //margin: EdgeInsets.only(top: SizeConfig.safeBlockVertical * 2, bottom: 29),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 0.0, horizontal: 5.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              // Icon(MaterialDesignIcons.local_activity,size: 14,color: BuytimeTheme.SymbolWhite),
+              Text(
+                '-${conventionHelper.getConventionDiscount(widget.serviceState, widget.businessId)}%',
+                style: TextStyle(fontFamily: BuytimeTheme.FontFamily, color: BuytimeTheme.TextWhite, fontWeight: FontWeight.w500, fontSize: 13),
+              ),
+              !widget.onlyIcon ?
+              Container(
+                margin: EdgeInsets.only(left: 5),
+                child: Text(
+                  AppLocalizations.of(context).promo.toUpperCase(),
+                  style: TextStyle(color: BuytimeTheme.TextWhite, fontSize: 12, fontFamily: BuytimeTheme.FontFamily, fontWeight: FontWeight.bold, letterSpacing: 1.25),
+                ),
+              ) : Container(),
+            ],
+          ),
+        ));
+  }
+}
